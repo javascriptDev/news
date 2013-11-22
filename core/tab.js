6ddcc17ffe012a48083ce508994cdd2bd3a0c5b1
JavascriptDev <627535440@qq.com>
2013-11-22 11:53:07 +0800

    base

--- core/tab.js
+++ core/tab.js
@@ -17,7 +17,7 @@ jex.define('tab', {
         var items = this.childs;
 
 
-        var info = jex.html.getInfo(this.alias),
+        var info = jex.html.getInfo(this.extend),
             dom = info.dom(),
             clsList = info.getClsList(),
             mainCls = clsList.main,
@@ -33,22 +33,26 @@ jex.define('tab', {
         var that = this;
         var itemContainer, barContainer;
         if (isItem) {
-            itemContainer = doucment.createElement('div');
-            itemContainer.className = jex.prefix + 'main-' + index;
+            itemContainer = document.createElement('div');
+            itemContainer.className = jex.prefix + 'main-' + itemContainer.childNodes.length;
             barContainer = document.createElement('div');
-            barContainer.className = jex.prefix + 'tab-' + index;
+            barContainer.className = jex.prefix + 'tab-' + barContainer.childNodes.length;
         }
 
         jex.each(items, function (item, index) {
-            if (item.items) {
-                that.setChildDom(item, main, bar, false);
-            }
-            if (itemContainer) {
-
-            }
-            if (barContainer) {
-
-            }
+//            if (item.items) {
+//                that.setChildDom(item, main, bars, false);
+//            }
+//            if (itemContainer) {
+//                var fun = jex.generateFc(jex.merge(jex.classManager.getModel(item.alias), item));
+//                itemContainer.appendChild(fun.element);
+//                main.appendChild(itemContainer);
+//            }
+//            if (barContainer) {
+//                var bar = document.createElement('div');
+//                bar.innerText = item.barText;
+//                bars.appendChild(bar);
+//            }
 
         });
     },
