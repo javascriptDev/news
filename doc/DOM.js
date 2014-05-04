d9dd9921bf10c73a88a0cb4198c096471f0680ce
JavascriptDev <627535440@qq.com>
2014-05-04 17:25:24 +0800

    添加 文档主内容模版

--- doc/DOM.js
+++ doc/DOM.js
@@ -2,10 +2,53 @@
  * Created by John on 2014/2/24.
  */
 var Component = {
+    tpl: '<div class="m-tool"><input type="text" id="search">' +
+        '<button id="getClass">搜索</button>' +
+        '</div>' +
+        '<div class="m-info">' +
+        '<div class="name">{name}</div>' +
+        '<div class="layer">{layer}</div>' +
+        '</div>' +
+        '<div class="demo">{demo}</div>' +
+        '<div class="property">{property}</div>' +
+        '<div class="method">{method}</div>' +
+        '<div class="event">{event}</div>',
+
+    replace: function (data) {
+        return   this.tpl.replace(/\{(.|\n|\r)*?\}/g, function (a) {
+            return data[a.replace('{', '').replace('}', '')];
+        })
+    },
     control: {
         viewport: function () {
 
-            return '<div></div>';
+            var data = {};
+            return Component.replace(data);
+        },
+        panel: function () {
+            var data = {};
+            return Component.replace(data);
+        },
+        list: function () {
+            var data = {};
+            return Component.replace(data);
         }
+    },
+    event: {
+        eventBase: function () {
+            var data = {};
+            return Component.replace(data);
+        }
+    },
+    store: {
+        storeMgr: function () {
+            var data = {};
+            return Component.replace(data);
+        },
+        store: function () {
+            var data = {};
+            return Component.replace(data);
+        }
+
     }
 }
--- doc/tree.js
+++ doc/tree.js
@@ -3,7 +3,7 @@
  */
 (function () {
 
-    var a = [
+    var data = [
         {
             node: 'control',
             items: [
@@ -52,46 +52,53 @@
 
     //点击具体组建的时候
     var leafFunc = function (dom) {
+        if (this.leafClick) {
+            this.leafClick({
+                type: dom.dataset['type'],
+                name: dom.dataset['name']
+            });
+        }
 
     }
 
-    var addEvent = function (dom) {
-        dom.onclick = function (e) {
+    var addEvent = function () {
+        var me = this;
+        this.el.onclick = function (e) {
             var cls = e.target.className;
             if (cls) {
                 if (cls == 'node-dom') {
                     nodeFuc(e.target);
                 } else if (cls == 'leaf-dom') {
-                    leafFunc(e.target);
-                }else if(cls=='icon'){
+                    leafFunc.call(me, e.target);
+                } else if (cls == 'icon') {
 
                 }
             }
         }
     }
-
     var nodeDom = function (text) {
         var a = document.createElement('div');
         a.className = 'node-dom';
         a.innerHTML = '<i class="icon expand"></i>' + text;
         return a;
     };
-    var leafDom = function (text) {
+    var leafDom = function (name, type) {
         var a = document.createElement('div');
         a.className = 'leaf-dom';
-        a.innerText = text;
+        a.innerText = name;
+        a.setAttribute('data-type', type);
+        a.setAttribute('data-name', name);
         return a;
     };
     var c = document.createElement('div');
     c.className = 'j-tree';
-    var tree = function (data) {
-        this.data = a;
+    var tree = function (cfg) {
+        this.data = data;
         this.el = c;
+        this.leafClick = cfg.leafClick || null;
     }
-
     tree.prototype.render = function (data) {
         var me = this, el = [];
-
         Array.prototype.forEach.call(data || this.data, function (item, index) {
             var area = document.createElement('div');
             area.className = 'area-type';
@@ -101,16 +108,12 @@
             leafColl.className = 'leaf-container';
 
             Array.prototype.forEach.call(item.items, function (leaf) {
-                leafColl.appendChild(leafDom(leaf.name));
+                leafColl.appendChild(leafDom(leaf.name,item.node));
             });
             area.appendChild(leafColl);
             me.el.appendChild(area);
         });
-
-
-        addEvent(this.el);
-
-
+        addEvent.call(this);
         return this.el;
     }
     window['Tree'] = tree;
