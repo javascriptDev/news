f359bc70240cc25fa59c4059b96203967f13b840
JavascriptDev <627535440@qq.com>
2014-05-05 17:46:58 +0800

    分离各个组建的描述数据

---
+++ doc/ComponentData.js
@@ -0,0 +1,90 @@
+/**
+ * Created by John on 2014/5/5.
+ */
+var data = {
+    control: {
+        viewport: function () {
+            var properties = [
+                {
+                    name: 'isComponet',
+                    defaultVal: 'true',
+                    dataType: 'boolean'
+                },
+                {
+                    name: 'config',
+                    defaultVal: '{}',
+                    dataType: 'Object'
+                }
+            ]
+            var method = [
+                {
+                    name: 'add',
+                    use: '添加一个组件到当前组件',
+                    params: '组建的实例对象',
+                    trn: 'undefined'
+
+                },
+                {
+                    name: 'getCld',
+                    use: '获取某个子对象组件',
+                    params: '组建的alias别名',
+                    trn: '组件对象'
+
+                }
+            ]
+            var event = [
+                {
+                    name: 'beforeRender',
+                    trigger: '渲染dom之前',
+                    param: 'null'
+                },
+                {
+                    name: 'render',
+                    trigger: '渲染dom',
+                    param: 'null'
+                },
+                {
+                    name: 'render',
+                    trigger: '渲染dom完',
+                    param: 'null'
+                }
+            ]
+
+            var data = {
+                name: 'viewport',
+                layer: '<div>控件的基类</div>',
+                demo: '<div>var a=jex.create("viewport");</div>',
+                property: new Component.List(Component.propertyTpl, properties).el,
+                method: new Component.List(Component.methodTpl, method).el,
+                event: new Component.List(Component.eventTpl, event).el
+            };
+            return Component.replace(Component.tpl, data);
+        },
+        panel: function () {
+            var data = {};
+            return Component.replace(Component.tpl, data);
+        },
+        list: function () {
+            var data = {};
+            return Component.replace(Component.tpl, data);
+        }
+    },
+    event: {
+        eventBase: function () {
+            var data = {};
+            return Component.replace(Component.tpl, data);
+        }
+    },
+    store: {
+        storeMgr: function () {
+            var data = {};
+            return Component.replace(Component.tpl, data);
+        },
+        store: function () {
+            var data = {};
+            return Component.replace(Component.tpl, data);
+        }
+
+    }
+
+}
